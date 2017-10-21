8183849cccbacdc957f10f29d250a37cf7ec1d75
zwang155 <31353792+zwang155@users.noreply.github.com>
2017-10-20 18:30:18 -0700

    Add files via upload

--- BSTree.java
+++ BSTree.java
@@ -1,137 +1,141 @@
-
 public class BSTree {
+
+	private class Node {
+
+		private String data;
+		private Node left;
+		private Node right;
+		private int count;
+
+		private Node(String d) {
+			data = d;
+			left = null;
+			right = null;
+			count = 1;
+		}
+	}
+
 	private Node root;
-	
+
 	public BSTree() {
 		root = null;
 	}
-	
-	public BSTree(String r){
+
+	public BSTree(String r) {
 		root = new Node(r);
 	}
-	
+
 	public void insert(String d) {
 		if (root == null)
 			root = new Node(d);
 		else
 			insertR(root, d);
 	}
+
 	public void insertR(Node n, String d) {
-		if (n.get().compareTo(d) > 0) {
-			if (n.left() == null)
-				n.setLeft(new Node(d));
+		if (n.data.compareTo(d) > 0) {
+			if (n.left == null)
+				n.left = new Node(d);
 			else
-				insertR(n.left(), d);
-		}
-		else if (n.get().compareTo(d) < 0) {
-			if (n.right() == null)
-				n.setRight(new Node(d));
+				insertR(n.left, d);
+		} else if (n.data.compareTo(d) < 0) {
+			if (n.right == null)
+				n.right = new Node(d);
 			else
-				insertR(n.right(), d);
-		}
-		else
-			n.add();
-			
+				insertR(n.right, d);
+		} else
+			n.count++;
+
 	}
-	
+
 	public boolean find(String d) {
 		return findR(root, d);
 	}
+
 	public boolean findR(Node n, String d) {
 		if (n == null)
 			return false;
-		else if (n.get().compareTo(d) > 0)
-			return findR(n.left(), d);
-		else if (n.get().compareTo(d) < 0)
-			return findR(n.right(), d);
+		else if (n.data.compareTo(d) > 0)
+			return findR(n.left, d);
+		else if (n.data.compareTo(d) < 0)
+			return findR(n.right, d);
 		return true;
 	}
-	
+
 	public void delete(String d) {
-		deleteR(root, d);
-	}
-	public void deleteR(Node n, String d) {
-		if (n.get().compareTo(d) > 0)
-			if (n.left() == null)
-				return;
-			else if (n.left().left() == null && n.left().right() == null && n.left().get().equals(d)) {
-				if (n.left().getCount() > 1)
-					n.left().remove();
-				else
-					n.setLeft(null);
-			}
-			else
-				deleteR(n.left(), d);
-		else if (n.get().compareTo(d) < 0)
-			if (n.right() == null)
-				return;
-			else if (n.right().left() == null && n.right().right() == null && n.right().get().equals(d)) {
-				if (n.right().getCount() > 1)
-					n.right().remove();
-				else
-					n.setRight(null);
-			}
-			else
-				deleteR(n.right(), d);
-		else if (n.getCount() > 1)
-			n.remove();
-		else {
-			if (n.right() != null) {
-				Node temp = n.right();
-				if (temp.left() == null) {
-					n.set(temp.get());
-					n.setCount(temp.getCount());
-					n.setRight(temp.right());
-				} else {
-					while (temp.left().left() != null) {
-						temp = temp.left();
-					}
-					n.set(temp.left().get());
-					n.setCount(temp.left().getCount());
-					temp.setLeft(temp.left().right());
-				}
-			} else if (n.left() != null) {
-				Node temp = n.left();
-				if (temp.right() == null) {
-					n.set(temp.get());
-					n.setCount(temp.getCount());
-					n.setLeft(temp.left());
-				} else {
-					while (temp.right().right() != null) {
-						temp = temp.right();
-					}
-					n.set(temp.right().get());
-					n.setCount(temp.right().getCount());
-					temp.setRight(temp.right().left());
-				}
-			} else
-				root = null;
-		}
+		root = deleteR(root, d);
 	}
 	
+	private Node replace(Node n) {
+		// find the Node to replace n and fix structure
+		if (n.right != null) {
+			Node temp = n.right;
+			if (temp.left == null)
+				n = temp;
+			else {
+				while (temp.left.left != null) {
+					temp = temp.left;
+				}
+				n.data = temp.left.data;
+				n.count = temp.left.count;
+				temp.left = temp.left.right;
+			}
+		} else if (n.left != null) {
+			Node temp = n.left;
+			if (temp.right == null)
+				n = temp;
+			else {
+				while (temp.right.right != null) {
+					temp = temp.right;
+				}
+				n.data = temp.right.data;
+				n.count = temp.right.count;
+				temp.right = temp.right.left;
+			}
+		} else
+			n = null;
+		return n;
+	}
+	
+	public Node deleteR(Node n, String d) {
+		if (n == null)
+			return null;
+		else if (n.data.compareTo(d) > 0)
+			n.left = deleteR(n.left, d);
+		else if (n.data.compareTo(d) < 0)
+			n.right = deleteR(n.right, d);
+		else if (n.count > 1)
+			n.count--;
+		else {
+			return replace(n);
+		}
+		return n;
+	}
+
 	public String toStringInOrder() {
 		return toStringInOrderR(root).substring(1);
 	}
+
 	public String toStringInOrderR(Node n) {
 		String s = "";
-		if (n.left() != null)
-			s += toStringInOrderR(n.left());
-		s += " " + n.get();
-		if (n.right() != null)
-			s += toStringInOrderR(n.right());
+		if (n.left != null)
+			s += toStringInOrderR(n.left);
+		s += " " + n.data;
+		if (n.right != null)
+			s += toStringInOrderR(n.right);
 		return s;
 	}
-	
+
 	public String toStringPreOrder() {
 		return toStringPreOrderR(root).substring(1);
 	}
+
 	public String toStringPreOrderR(Node n) {
-		String s = " " + n.get();
-		if (n.left() != null)
-			s += toStringPreOrderR(n.left());
-		if (n.right() != null)
-			s += toStringPreOrderR(n.right());
+		String s = " " + n.data;
+		if (n.left != null)
+			s += toStringPreOrderR(n.left);
+		if (n.right != null)
+			s += toStringPreOrderR(n.right);
 		return s;
 	}
-	
 }
